063262389c09688e18bb00afeaa92aa1acbdb6f7
Olivier <olivierhoogers@gmail.com>
2022-11-15 13:58:26 +0100

    auto points
    auto points toegevoegd

--- Assets/Scenes/BasicClickSystem.cs
+++ Assets/Scenes/BasicClickSystem.cs
@@ -6,20 +6,29 @@ using TMPro;
 public class BasicClickSystem : MonoBehaviour
 {
     [SerializeField] private float points;
+    [SerializeField] private float autoPointsPS = 0f;
     [SerializeField] public TextMeshProUGUI displayPoints;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] public float[] increaseAutoPoints = { 50f, 250f , 500f , 1000f, 10000f, 100000f, 500000f, 1000000f, };
+    [SerializeField] public int x = 0;
+   // [SerializeField] public bool enoughPoints = false;
+    
+ 
     void Update()
     {
-        displayPoints.text = "Score: " + points.ToString();
+        displayPoints.text = "Score: " + System.Math.Round(points, 1).ToString();
+
         if (Input.GetMouseButtonDown(0))
         {
             points++;
+        }
+        if(points <= increaseAutoPoints[x])
+        {
+           points += 1 * Time.deltaTime * autoPointsPS;          
+        }
+        else
+        {
+            x++;
+            autoPointsPS++;
         }
     }
 }
