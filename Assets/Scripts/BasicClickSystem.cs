1af905f2b4922d40f4ec2a06ebbf7bb9f9d3b6f8
Olivier <olivierhoogers@gmail.com>
2022-12-02 10:59:35 +0100

    button geadd voor klikken
    leuk toch?

--- Assets/Scripts/BasicClickSystem.cs
+++ Assets/Scripts/BasicClickSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class BasicClickSystem : MonoBehaviour
@@ -10,18 +11,16 @@ public class BasicClickSystem : MonoBehaviour
     [SerializeField] public TextMeshProUGUI displayPoints, displayPoints2;
     [SerializeField] public float[] increaseAutoPoints = { 50f, 250f , 500f , 1000f, 10000f, 100000f, 500000f, 1000000f, };
     [SerializeField] public int x = 0;
-   // [SerializeField] public bool enoughPoints = false;
-    
- 
+    [SerializeField] Button elonMaskButton;
+    // [SerializeField] public bool enoughPoints = false;
+
+
     void Update()
     {
         displayPoints.text = "Score: " + System.Math.Round(points, 1).ToString();
         displayPoints2.text = "Score: " + System.Math.Round(points, 1).ToString();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            points++;
-        }
+        
     /*    if(points <= increaseAutoPoints[x])
         {
            points += 1 * Time.deltaTime * autoPointsPS;          
@@ -32,5 +31,9 @@ public class BasicClickSystem : MonoBehaviour
             autoPointsPS++;
         }
     */
+    }
+    public void Buttonclick()
+    {
+        points++;
     }
 }
