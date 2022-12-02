793ac8ebd07ee68b0f3f6cf6169f4f49f5dba031
thommell <thomslaghekke@outlook.com>
2022-12-02 11:16:21 +0100

    test push to fix errors

--- Assets/Scripts/BasicClickSystem.cs
+++ Assets/Scripts/BasicClickSystem.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 
 public class BasicClickSystem : MonoBehaviour
@@ -11,16 +10,18 @@ public class BasicClickSystem : MonoBehaviour
     [SerializeField] public TextMeshProUGUI displayPoints, displayPoints2;
     [SerializeField] public float[] increaseAutoPoints = { 50f, 250f , 500f , 1000f, 10000f, 100000f, 500000f, 1000000f, };
     [SerializeField] public int x = 0;
-    [SerializeField] Button elonMaskButton;
-    // [SerializeField] public bool enoughPoints = false;
-
-
+   // [SerializeField] public bool enoughPoints = false;
+    
+ 
     void Update()
     {
         displayPoints.text = "Score: " + System.Math.Round(points, 1).ToString();
         displayPoints2.text = "Score: " + System.Math.Round(points, 1).ToString();
 
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            points++;
+        }
     /*    if(points <= increaseAutoPoints[x])
         {
            points += 1 * Time.deltaTime * autoPointsPS;          
@@ -31,9 +32,5 @@ public class BasicClickSystem : MonoBehaviour
             autoPointsPS++;
         }
     */
-    }
-    public void Buttonclick()
-    {
-        points++;
     }
 }
