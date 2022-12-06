7b17579e544a9a518697125b187958bb5d6e283d
thommell <thomslaghekke@outlook.com>
2022-12-06 15:36:59 +0100

    Revert "Merge branch 'BugBranchTest' into DevBranch2"
    This reverts commit 35e88abdc39f30858acf9cb0044da310b9d9e134, reversing
    changes made to 438d8273387fc7de8e68ebcdb6f419f2f91f98a9.

--- Assets/Victor/Scripts Victor/MinigameScore.cs
+++ Assets/Victor/Scripts Victor/MinigameScore.cs
@@ -8,15 +8,10 @@ public class MinigameScore : MonoBehaviour
     [SerializeField] public TextMeshProUGUI scoreText;
     [SerializeField] public TimedEventButtonScript _tes;
     public int scoreInt;
-    public int wonMinigames = 0;
 
     // Update is called once per frame
     private void Update()
     { 
-        if(scoreInt >= 10)
-        {
-            wonMinigames++;
-        }
             scoreText.text = scoreInt + "/10 \nbuttons";
     }
 }
