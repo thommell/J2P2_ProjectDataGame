ff6ab530d2c45c70313be356377453b57a65486e
thommell <thomslaghekke@outlook.com>
2022-12-06 12:57:02 +0100

    Merge branch 'Thom2' into Development-Branch

--- Assets/Scripts/BasicClickSystem.cs
+++ Assets/Scripts/BasicClickSystem.cs
@@ -2,35 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class BasicClickSystem : MonoBehaviour
 {
     [SerializeField] public float points;
- //  [SerializeField] private float autoPointsPS = 0f;
     [SerializeField] public TextMeshProUGUI displayPoints, displayPoints2;
-    [SerializeField] public float[] increaseAutoPoints = { 50f, 250f , 500f , 1000f, 10000f, 100000f, 500000f, 1000000f, };
-    [SerializeField] public int x = 0;
-   // [SerializeField] public bool enoughPoints = false;
-    
- 
+    [SerializeField] Button elonMaskButton;
+
+
     void Update()
     {
         displayPoints.text = "Score: " + System.Math.Round(points, 1).ToString();
         displayPoints2.text = "Score: " + System.Math.Round(points, 1).ToString();
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            points++;
-        }
-    /*    if(points <= increaseAutoPoints[x])
-        {
-           points += 1 * Time.deltaTime * autoPointsPS;          
-        }
-        else
-        {
-            x++;
-            autoPointsPS++;
-        }
-    */
+ 
+    }
+    public void buttonClick()
+    {
+        points++;
     }
 }
