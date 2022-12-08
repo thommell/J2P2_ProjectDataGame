3aa63bd45a89b5beb948a90ea3ec7dee8118a38e
IXYamato <victorhessing21@gmail.com>
2022-12-08 13:34:16 +0100

    planet renderer v1
    :)

--- Assets/Victor/Scripts Victor/MinigameScore.cs
+++ Assets/Victor/Scripts Victor/MinigameScore.cs
@@ -8,10 +8,16 @@ public class MinigameScore : MonoBehaviour
     [SerializeField] public TextMeshProUGUI scoreText;
     [SerializeField] public TimedEventButtonScript _tes;
     public int scoreInt;
+    public int wonMinigames;
 
     // Update is called once per frame
     private void Update()
     { 
             scoreText.text = scoreInt + "/10 \nbuttons";
+
+            if(scoreInt >= 10)
+            {
+            wonMinigames++;
+            }
     }
 }
